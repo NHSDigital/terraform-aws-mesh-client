db5d2b36335f20c45697985865128c8036eab9d6
jacobgill1 <jacob.gill1@nhs.net>
2023-05-30 16:53:05 +0100

    add poll mailbox tests

---
+++ tests/mesh_poll_mailbox_application_test.py
@@ -0,0 +1,89 @@
+""" Testing MeshPollMailbox application """
+import json
+from http import HTTPStatus
+from unittest import mock
+
+import boto3
+import requests_mock
+
+from moto import mock_s3, mock_ssm, mock_stepfunctions, mock_secretsmanager
+from mesh_client_aws_serverless.mesh_poll_mailbox_application import (
+    MeshPollMailboxApplication,
+)
+from tests.mesh_testing_common import (
+    MeshTestCase,
+    MeshTestingCommon,
+)
+
+
+class TestMeshPollMailboxApplication(MeshTestCase):
+    """Testing MeshPollMailbox application"""
+
+    @mock.patch.dict("os.environ", MeshTestingCommon.os_environ_values)
+    def setUp(self):
+        """Override setup to use correct application object"""
+        super().setUp()
+        self.app = MeshPollMailboxApplication()
+        self.environment = self.app.system_config["Environment"]
+
+    @mock_secretsmanager
+    @mock_ssm
+    @mock_s3
+    @mock_stepfunctions
+    @requests_mock.Mocker()
+    def test_mesh_poll_mailbox_happy_path(self, mock_response):
+        """Test the lambda"""
+        mailbox_id = MeshTestingCommon.TEST_MAILBOX_ID1
+
+        # Mock response from MESH server
+        mock_response.get(
+            f"/messageexchange/{mailbox_id}/inbox",
+            text=json.dumps(
+                {
+                    "messages": [
+                        MeshTestingCommon.KNOWN_MESSAGE_ID1,
+                        MeshTestingCommon.KNOWN_MESSAGE_ID2,
+                        MeshTestingCommon.KNOWN_MESSAGE_ID3,
+                    ]
+                }
+            ),
+        )
+
+        mock_input = {"mailbox": mailbox_id}
+        s3_client = boto3.client("s3", region_name="eu-west-2")
+        ssm_client = boto3.client("ssm", region_name="eu-west-2")
+        MeshTestingCommon.setup_mock_aws_s3_buckets(self.environment, s3_client)
+        MeshTestingCommon.setup_mock_aws_ssm_parameter_store(
+            self.environment, ssm_client
+        )
+        sfn_client = boto3.client("stepfunctions", region_name="eu-west-2")
+        response = MeshTestingCommon.setup_step_function(
+            sfn_client,
+            self.environment,
+            self.app.get_messages_step_function_name,
+        )
+        try:
+            response = self.app.main(
+                event=mock_input, context=MeshTestingCommon.CONTEXT
+            )
+        except Exception as e:  # pylint: disable=broad-except
+            # need to fail happy pass on any exception
+            self.fail(f"Invocation crashed with Exception {str(e)}")
+
+        self.assertEqual(HTTPStatus.OK.value, response["statusCode"])
+        # check 3 messages received
+        self.assertEqual(3, response["body"]["message_count"])
+        # check first message format in message_list
+        self.assertEqual(
+            MeshTestingCommon.KNOWN_MESSAGE_ID1,
+            response["body"]["message_list"][0]["body"]["message_id"],
+        )
+        self.assertEqual(False, response["body"]["message_list"][0]["body"]["complete"])
+        self.assertEqual(
+            mailbox_id, response["body"]["message_list"][0]["body"]["dest_mailbox"]
+        )
+        # check the correct logs exist
+        self.assertLogs("LAMBDA0001", level="INFO")
+        self.assertLogs("LAMBDA0002", level="INFO")
+        self.assertLogs("LAMBDA0003", level="INFO")
+        self.assertLogs("MESHPOLL0001", level="INFO")
