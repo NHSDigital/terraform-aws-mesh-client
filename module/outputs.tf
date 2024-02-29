2316b3858b25acf9304b364a2660ebc783265b99
Jon Marston <john.marston1@nhs.net>
2024-02-29 15:56:49 +0000

    EM-781. Slight re-order of the outputs to group resources more clearly.

--- module/outputs.tf
+++ module/outputs.tf
@@ -26,8 +26,15 @@ output "step_function_get_messages_arn" {
   value = aws_sfn_state_machine.get_messages.arn
 }
 
+output "stepfunction_get_messages_log_group_name" {
+  value = aws_cloudwatch_log_group.get_messages.name
+}
 output "step_function_send_messages_arn" {
   value = aws_sfn_state_machine.send_message.arn
+}
+
+output "stepfunction_send_message_log_group_name" {
+  value = aws_cloudwatch_log_group.send_message.name
 }
 
 output "lambda_check_send_parameters_log_group_name" {
@@ -44,12 +51,4 @@ output "lambda_poll_mailbox_log_group_name" {
 
 output "lambda_send_message_chunk_log_group_name" {
   value = aws_cloudwatch_log_group.send_message_chunk.name
-}
-
-output "stepfunction_get_messages_log_group_name" {
-  value = aws_cloudwatch_log_group.get_messages.name
-}
-
-output "stepfunction_send_message_log_group_name" {
-  value = aws_cloudwatch_log_group.send_message.name
 }
